73a92b308a548be3ad07a66a465cfea35e12f9d0
mimimixer <113891038+mimimixer@users.noreply.github.com>
2023-03-12 00:58:02 +0100

    filterGenre works fine, 7 tests, only "noClick" not tested because click is read in searchBtn-ActionHandler, where also filter-method is called - idk how2

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -47,112 +47,125 @@ class HomeControllerTest {
 
     }
 
-  /*  @Test
-  //hier problem dass Filter in initialize:searchBtn gesetzt wird, also kann ich nicht hier testen, muss neu
-    public void filter_movies_by_genre_nothing_chosen() {
-        HomeController hc = new HomeController();
-        List<Movie> movieList = new ArrayList<>();
-
-        movieList.add(new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER)));
-        movieList.add(new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE)));
-        movieList.add(new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY)));
-        movieList.add(new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY)));
-
-        List<Movie> expected = movieList;
-        List<Movie> actual = hc.filterMoviesByGenre(movieList, null);
-
-        assertEquals(expected, actual);
-    }
-*/
     @Test
-    void filter_movies_by_genre_show_all() {
-        HomeController hc = new HomeController();
-        List<Movie> movieList = new ArrayList<>();
-
-        movieList.add(new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER)));
-        movieList.add(new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE)));
-        movieList.add(new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY)));
-        movieList.add(new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY)));
-
-        List<Movie> expected = movieList;
-        List<Movie> actual = hc.filterMoviesByGenre(movieList, Genre.SHOW_ALL_GENRES);
-
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void filter_movies_by_genre_WESTERN() {
+    void filter_movies_by_genre_show_all_movies_clicked() {
         HomeController hc = new HomeController();
         List<Movie> movieList = new ArrayList<>();
 
         Movie movie1= new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER));
         Movie movie2= new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE));
-        Movie movie3= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
-        Movie movie4= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
-        Collections.addAll(movieList, movie1,movie2,movie3,movie4);
+        Movie movie3= new Movie("Glass Onion", "blah blah", Arrays.asList());
+        Movie movie4= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
+        Movie movie5= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
+        Collections.addAll(movieList, movie1,movie2,movie3,movie4,movie5);
+
+        List<Movie> expected = new ArrayList<>();
+        Collections.addAll(expected, movie1,movie2,movie3,movie4,movie5);
+
+        List<Movie> actual = hc.filterMoviesByGenre(movieList, Genre.SHOW_ALL_GENRES);
+
+        assertEquals(expected, actual, "Not all Movies shown!");
+    }
+    @Test
+    void filter_movies_by_genre_no_movie_with_that_genre() {
+        HomeController hc = new HomeController();
+        List<Movie> movieList = new ArrayList<>();
+
+        Movie movie1= new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER));
+        Movie movie2= new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE));
+        Movie movie4= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
+        Movie movie5= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
+        Collections.addAll(movieList, movie1,movie2,movie5,movie4);
 
         List<Movie> expected = new ArrayList<>();
         Collections.addAll(expected);
 
         List<Movie> actual = hc.filterMoviesByGenre(movieList, Genre.WESTERN);
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "There is no movie with that genre!");
     }
-
     @Test
-    void filter_movies_by_genre_DRAMA() {
+    void filter_movies_by_genre_no_movie_with_that_genre_show_movies_with_no_genre() {
         HomeController hc = new HomeController();
         List<Movie> movieList = new ArrayList<>();
 
         Movie movie1= new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER));
         Movie movie2= new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE));
-        Movie movie3= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
-        Movie movie4= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
-        Collections.addAll(movieList, movie1,movie2,movie3,movie4);
+        Movie movie3= new Movie("Glass Onion", "blah blah", Arrays.asList());
+        Movie movie4= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
+        Movie movie5= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
+        Collections.addAll(movieList, movie1,movie2,movie3,movie4, movie5);
 
         List<Movie> expected = new ArrayList<>();
-        Collections.addAll(expected,movie1,movie2);
+        Collections.addAll(expected, movie3);
+
+        List<Movie> actual = hc.filterMoviesByGenre(movieList, Genre.WESTERN);
+
+        assertEquals(expected, actual, "Movie with no genre should also be shown");
+    }
+
+    @Test
+    void filter_movies_by_genre_only_two_movies_have_genre_not_all() {
+        HomeController hc = new HomeController();
+        List<Movie> movieList = new ArrayList<>();
+
+        Movie movie1= new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER));
+        Movie movie2= new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE));
+        Movie movie3= new Movie("Glass Onion", "blah blah", Arrays.asList());
+        Movie movie4= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
+        Movie movie5= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
+        Collections.addAll(movieList, movie1,movie2,movie3,movie4,movie5);
+
+        List<Movie> expected = new ArrayList<>();
+        Collections.addAll(expected,movie1,movie2,movie3);
         List<Movie> actual = hc.filterMoviesByGenre(movieList, Genre.DRAMA);
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "Not all movies with selected genre shown - 2 movies");
     }
 
     @Test
-    void filter_movies_by_genre_ACTION() {
+    void filter_movies_by_genre_all_movies_have_genre() {
         HomeController hc = new HomeController();
         List<Movie> movieList = new ArrayList<>();
 
         Movie movie1= new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER));
         Movie movie2= new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE));
-        Movie movie3= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
-        Movie movie4= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
-        Collections.addAll(movieList, movie1,movie2,movie3,movie4);
+        Movie movie3= new Movie("Glass Onion", "blah blah", Arrays.asList());
+        Movie movie4= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
+        Movie movie5= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
+        Collections.addAll(movieList, movie1,movie2,movie3,movie4, movie5);
 
         List<Movie> expected = new ArrayList<>();
-        Collections.addAll(expected,movie1,movie2,movie3,movie4);
+        Collections.addAll(expected,movie1,movie2,movie4,movie5,movie3);
 
         List<Movie> actual = hc.filterMoviesByGenre(movieList, Genre.ACTION);
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual, "Not all movies with selected genre shown - all movies");
     }
 
     @Test
-    void filter_movies_by_genre_FANTASY() {
+    void filter_movies_by_genre_only_one_movie_has_that_genre() {
         HomeController hc = new HomeController();
         List<Movie> movieList = new ArrayList<>();
 
         Movie movie1= new Movie("Anastasia", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.THRILLER));
         Movie movie2= new Movie("Dingaling", "blah blah", Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ADVENTURE));
-        Movie movie3= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
-        Movie movie4= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
-        Collections.addAll(movieList, movie1,movie2,movie3,movie4);
+        Movie movie3= new Movie("Glass Onion", "blah blah", Arrays.asList());
+        Movie movie4= new Movie("Zimmbabwe", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.FANTASY));
+        Movie movie5= new Movie("Cops", "blah blah", Arrays.asList(Genre.ACTION, Genre.ADVENTURE, Genre.DOCUMENTARY));
+        Collections.addAll(movieList, movie1,movie2,movie3,movie4, movie5);
 
         List<Movie> expected = new ArrayList<>();
-        Collections.addAll(expected,movie3);
+        Collections.addAll(expected, movie4, movie3);
 
         List<Movie> actual = hc.filterMoviesByGenre(movieList, Genre.FANTASY);
 
-        assertEquals(expected, actual);
+        assertEquals(expected, actual,"Not all movies with selected genre shown - 1 movie");
     }
 
+    @Test
+    void filter_movies_by_genre_no_genre_selected() {}
+    //hier problem dass Filter in initialize:searchBtn - abhängig von Eingabe - gesetzt wird, also kann ich nicht hier testen
+
+//did I test configuration settings when testing different possibilities?
 }
