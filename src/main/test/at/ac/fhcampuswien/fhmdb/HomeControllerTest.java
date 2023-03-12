c449ed7dc3767671a3e9dddc06d1efc77996ca50
Janella Gatmaitan <janella.gatmaitan@gmail.com>
2023-03-12 23:27:46 +0100

    Surefire plugin updated

--- src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
+++ src/main/test/at/ac/fhcampuswien/fhmdb/HomeControllerTest.java
@@ -4,6 +4,10 @@ import at.ac.fhcampuswien.fhmdb.models.Movie;
 import at.ac.fhcampuswien.fhmdb.models.Genre;
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import java.net.URL;
 import java.util.*;
